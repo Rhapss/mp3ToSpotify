f09e33e874240c62269e24e0d02b965d8292f5c1
Rhapss <45916288+Rhapss@users.noreply.github.com>
2020-09-01 00:02:59 +0300

    Initial commit

---
+++ mp3ToSpotify/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Numerics;
+
+namespace mp3ToSpotify
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var dir = new DirectoryInfo(@"F:\Music");
+            var allNames = new List< string >();
+
+            foreach (FileInfo file in dir.GetFiles())
+            {
+                allNames.Add(Path.GetFileNameWithoutExtension(file.FullName));
+            }
+            foreach (string file in allNames)
+            {
+                Console.WriteLine(file);
+            }
+        }
+    }
+}
