6e843fa947bba0e69d640769c72617a979f2b771
Rhapss <45916288+Rhapss@users.noreply.github.com>
2020-09-01 00:17:26 +0300

    v0.1
    -Created finding directory path
    -Getting fale names and put the into variable
    -Start trying clear names from trash chars like "_ , - , 0-9 nums etc."

--- mp3ToSpotify/Program.cs
+++ mp3ToSpotify/Program.cs
@@ -10,16 +10,18 @@ namespace mp3ToSpotify
     {
         static void Main(string[] args)
         {
-
+            char[] delimiterChars = { ' ', ',', '.', ':', '_', '0','1','2','3','4','5','6','7','8','9', '-', '\t' };
             var dir = new DirectoryInfo(@"F:\Music");
-            var allNames = new List< string >();
-
+            var allNames = new List<string>();
+            int i = 0;
             foreach (FileInfo file in dir.GetFiles())
             {
                 allNames.Add(Path.GetFileNameWithoutExtension(file.FullName));
             }
             foreach (string file in allNames)
             {
+                allNames[i].Replace('_', ' ');
+                i++;
                 Console.WriteLine(file);
             }
         }
