33f33cb378807f367ca7b8ad2867d48cbfde818d
Rhapss <45916288+Rhapss@users.noreply.github.com>
2020-09-16 20:01:59 +0300

    v1.1
    -Added comments
    -Fix that file in filestream didnt close
    -Implementet that fill will be rewrited if same file is already created

--- mp3ToSpotify/Program.cs
+++ mp3ToSpotify/Program.cs
@@ -2,7 +2,7 @@
 using System.IO;
 using System.Text;
 using TagLib;
-using File = TagLib.File;
+//using File = TagLib.File;
 
 namespace mp3ToSpotify
 {
@@ -10,24 +10,37 @@ namespace mp3ToSpotify
     {
         private static void Main()
         {
+            //Директория с музыкой
             var dir = new DirectoryInfo(@"F:\Music");
 
+            //Экземляр потока файлов
             FileStream fs = null;
-            File tagFile = null;
-            fs = new FileStream(@"F:\music3.txt", FileMode.CreateNew);
+
+            //Экземпляр класса для ображениям к тегам
+            TagLib.File tagFile = null;
+
+            //Создаем поток данных в указаный файл
+            fs = new FileStream(@"F:\music10.txt", FileMode.Create);
+
             var sw = new StreamWriter(fs, Encoding.UTF8);
             
+            //За каждую итерацию мы перебираем каждый последующий файл, подставляя путь к каждому последующему файлу в
+            //tagFile. После обращаясь уже к самому файлу берём тег исполнителя и названия песни и вписываем в файл построчно.
             foreach (var file in dir.GetFiles("*.mp3"))
-                //Console.WriteLine(file);
                 try
                 {
-                    tagFile = File.Create(file.FullName);
+                    tagFile = TagLib.File.Create(file.FullName);
                     sw.WriteLine("{0} - {1}", string.Join(", ", tagFile.Tag.Performers), tagFile.Tag.Title);
                 }
                 catch (CorruptFileException)
                 {
                     Console.WriteLine("Error!");
                     throw;
+                }
+                finally
+                {
+                    sw.Close();
+                    fs.Close();
                 }
             Console.WriteLine("Finish!");
         }
